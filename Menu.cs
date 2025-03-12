afebaa5f5c68e142386309e35acbe5975ef0e813
cayque.mantesso <cayque.mantesso@csn.com.br>
2025-03-12 14:18:22 -0300

    adicionando a substituicao de caracteres

--- Menu.cs
+++ Menu.cs
@@ -103,7 +103,7 @@ namespace EditorHtml
         {
             switch(option){
                 case 1:
-                Console.WriteLine("Editor"); break;
+                Editor.Show(); break;
                 case 2:
                 Console.WriteLine("View"); break;
                 case 0:
